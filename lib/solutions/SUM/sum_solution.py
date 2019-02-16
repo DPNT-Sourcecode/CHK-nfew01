2fedd3c50e2e9ac365e9feff0ab2d4cb10acdde1
sbx_user1060 <sbx_user1060@ip-10-129-203-245.eu-west-2.compute.internal>
2019-02-16 23:06:30 +0000

    Sat Feb 16 23:06:30 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,8 @@
-# noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     if isinstance(x, int) and isinstance(y, int):
         return x+y
     return '{} and {} must be integers in the range 1-100'.format(x, y)
+
 
 
 
