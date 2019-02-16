0b9a39e4bbcab9188cbd6e2d363186c3b19e8e38
sbx_user1060 <sbx_user1060@ip-10-129-203-245.eu-west-2.compute.internal>
2019-02-16 22:51:29 +0000

    Sat Feb 16 22:51:29 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    if isinstance(x, int) and isinstance(y, int):
+        return x+y
+    return
     raise NotImplementedError()
+
