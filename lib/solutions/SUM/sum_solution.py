f1c9207bfd62c5f24b4ee6bb9d4c2578ed54c4df
sbx_user1060 <sbx_user1060@ip-10-129-203-245.eu-west-2.compute.internal>
2019-02-16 22:54:30 +0000

    Sat Feb 16 22:54:30 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if isinstance(x, int) and isinstance(y, int):
+    if (isinstance(x, int) and isinstance(y, int)) and x,y in range(101):
         return x+y
-    return
+    return '{} and {} must be integers in the range 1-100'.format(x, y)
     raise NotImplementedError()
+
 
