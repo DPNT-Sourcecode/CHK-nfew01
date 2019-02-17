0f6673e5bb063f13544125c9a9ce885e110c7981
sbx_user1059 <sbx_user1059@ip-10-128-63-232.eu-west-2.compute.internal>
2019-02-17 16:36:51 +0000

    Sun Feb 17 16:36:51 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    if isinstance(friend_name, str):
+        return 'Hello {}'.format(friend_name)
+    return 'input must be a string'
+
+
