ee9e97584f23d60090b258bc7cbd00d6cff05a5b
sbx_user1059 <sbx_user1059@ip-10-128-212-107.eu-west-2.compute.internal>
2019-02-17 15:45:11 +0000

    Sun Feb 17 15:45:11 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,5 @@
 def compute(x, y):
-    if isinstance(x, int) and isinstance(y, int):
+    if (isinstance(x, int) and isinstance(y, int)) and (0 <= x and y <= 100):
         return x+y
     return 'inputs must be integers in the range 1-100'
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,10 @@
-from solutions.SUM import sum_solution
+import unittest
+from lib.solutions.SUM import sum_solution
 
 
-class TestSum():
+class TestSum(unittest.TestCase):
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        self.assertEqual(sum_solution.compute(1, 2), 3)
+        self.assertEqual(sum_solution.compute(1.03, 2.089), 'inputs must be integers in the range 1-100')
+        self.assertEqual(sum_solution.compute(513, 209), 'inputs must be integers in the range 1-100')
+        self.assertEqual(sum_solution.compute(-3, -5), 'inputs must be integers in the range 1-100')
