1a9cd216e4149ed8aa246230138e3e7fc5b6ce16
sbx_user1059 <sbx_user1059@ip-10-128-63-232.eu-west-2.compute.internal>
2019-02-17 16:39:51 +0000

    Sun Feb 17 16:39:51 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,8 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if isinstance(friend_name, str):
-        return 'Hello {}'.format(friend_name)
-    return 'input must be a string'
+        return 'Hello world'
 
+print(hello(['Craftsman']))
 
