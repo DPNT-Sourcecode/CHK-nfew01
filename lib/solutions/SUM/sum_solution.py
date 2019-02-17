814c37c8a450906840283db8dd107b0782246ccf
sbx_user1059 <sbx_user1059@ip-10-128-212-107.eu-west-2.compute.internal>
2019-02-17 15:34:47 +0000

    Sun Feb 17 15:34:47 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,4 @@
 def compute(x, y):
     if isinstance(x, int) and isinstance(y, int):
         return x+y
-    return '{} and {} must be integers in the range 1-100'.format(x, y)
-
-
-
-
+    return 'inputs must be integers in the range 1-100'
