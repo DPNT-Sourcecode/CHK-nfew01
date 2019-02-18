2b5014a6254e0e20cd2fde3f8a8a2f4904fbee52
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 14:09:56 +0000

    Mon Feb 18 14:09:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,15 +6,19 @@ def checkout(skus):
     total_checkout = []
     item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
-
-    #dealing with single items
     for sku in skus:
         if sku in item.keys():
             total_checkout.append(item[sku])
+        else:
+            return -1
 
     return sum(total_checkout)
 
 
 print(checkout('AA'))
 print(checkout("BBB"))
+print(checkout("-"))
+print(checkout(""))
+print(checkout("AxA"))
+print(checkout("ABCa"))
 
