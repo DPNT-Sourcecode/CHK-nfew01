c8173ba205f7127c077c698408922e48022f6981
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 13:35:12 +0000

    Mon Feb 18 13:35:12 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,8 @@
 def checkout(skus):
     total_checkout = []
     item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    item_special_offers = {'3A': 130, '2B': 45, '2C': 30, '2D': 25}
+
 
     #dealing with single items
     for sku in skus:
@@ -12,7 +14,7 @@ def checkout(skus):
             total.append(item[sku])
 
     #dealing with special offers
-    
+
 
 
 print(checkout('A,B,C,D'))
