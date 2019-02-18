e027bf70014d00c29e07e0f4ff0aa824570956bd
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 14:06:56 +0000

    Mon Feb 18 14:06:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,21 +5,16 @@
 def checkout(skus):
     total_checkout = []
     item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    item_special_offers = {'3A': 130, '2B': 45, '2C': 30, '2D': 25}
 
 
     #dealing with single items
     for sku in skus:
-        if sku == item.keys():
-            total_checkout.append(item[sku])
-
-    #dealing with special offers
-
-    for sku in skus:
-        if sku == item_special_offers.keys():
+        if sku in item.keys():
             total_checkout.append(item[sku])
 
     return sum(total_checkout)
 
-print(checkout('A,B,C,D'))
-print(checkout('3A,2B'))
+
+print(checkout('AA'))
+print(checkout("BBB"))
+
