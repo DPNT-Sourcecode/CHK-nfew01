b3ec38e1a06eb56d7a40949bfb6291f156aa07c7
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 13:47:13 +0000

    Mon Feb 18 13:47:13 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,18 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    total_checkout = []
-    item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    item_special_offers = {'3A': 130, '2B': 45, '2C': 30, '2D': 25}
+    # total_checkout = []
+    # item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    # item_special_offers = {'3A': 130, '2B': 45, '2C': 30, '2D': 25}
+    #
+    #
+    # #dealing with single items
+    # for sku in skus:
+    #     if sku in item.keys():
+    #         total.append(item[sku])
+    #
+    # #dealing with special offers
+    #
 
-
-    #dealing with single items
     for sku in skus:
-        if sku in item.keys():
-            total.append(item[sku])
-
-    #dealing with special offers
-    
-
+        if sku.startswith('3'):
+            
 
 print(checkout('A,B,C,D'))
