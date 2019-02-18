fc3102334faac9a0d691eceb2335e6ab7c9b0049
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 14:20:28 +0000

    Mon Feb 18 14:20:28 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,8 +9,6 @@ def checkout(skus):
     if not skus:
         return -1
 
-
-
     for sku in skus:
         if sku in item.keys():
             total_checkout.append(item[sku])
@@ -18,14 +16,6 @@ def checkout(skus):
             return -1
 
     return sum(total_checkout)
-
-
-print(checkout('AA'))
-print(checkout("BBB"))
-print(checkout("-"))
-print(checkout(""))
-print(checkout("AxA"))
-print(checkout("ABCa"))
 
 
 
