3cbd4bf1153a80720b404894679bb86214462940
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 13:26:12 +0000

    Mon Feb 18 13:26:12 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     for sku in skus:
+        if sku in item.keys():
+            
+
 
 
 
