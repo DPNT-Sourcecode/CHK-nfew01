73f435af3b62c1828863c435a16e5657e8d6fb6c
sbx_user1060 <sbx_user1060@ip-10-128-147-43.eu-west-2.compute.internal>
2019-02-18 20:12:24 +0000

    Mon Feb 18 20:12:24 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,16 +3,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    total_checkout = []
-    item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    # total_checkout = []
+    # item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    #
+    # if not skus:
+    #     return 0
+    #
+    # for sku in skus:
+    #     if sku in item.keys():
+    #         total_checkout.append(item[sku])
+    #     else:
+    #         return -1
+    #
+    # return sum(total_checkout)
 
-    if not skus:
-        return 0
 
-    for sku in skus:
-        if sku in item.keys():
-            total_checkout.append(item[sku])
-        else:
-            return -1
-
-    return sum(total_checkout)
+    occurence_of_A 
