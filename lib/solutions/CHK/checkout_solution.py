fb6cd8da213c15206ead109615fc48f6a2ddd0e0
sbx_user1060 <sbx_user1060@ip-10-128-147-43.eu-west-2.compute.internal>
2019-02-18 20:21:24 +0000

    Mon Feb 18 20:21:24 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,19 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # total_checkout = []
-    # item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    #
-    # if not skus:
-    #     return 0
-    #
-    # for sku in skus:
-    #     if sku in item.keys():
-    #         total_checkout.append(item[sku])
-    #     else:
-    #         return -1
-    #
-    # return sum(total_checkout)
+    total_checkout = 0
+    occurrence_of_A = skus.count('A')
+    occurrence_of_B = skus.count('B')
 
+    if occurrence_of_A % 3 == 0:
+        total_checkout += 130 * occurrence_of_A/3
 
-    occurence_of_A 
+    elif (occurrence_of_A + 1) % 3 == 0:
+        total_checkout += checkout((occurrence_of_A + 1)*'A')-30
+
+    elif (occurrence_of_A - 1) % 3 == 0:
+        total_checkout += checkout((occurrence_of_A-1)*'A') + 50
+
+    if occurrence_of_B % 2 == 0:
+        total_checkout += 45 * occurrence_of_B/2
+
+    else:
+        total_checkout += checkout((occurrence_of_B-1)*'B') + 30
+
+    return total_checkout
+
+print(checkout('AAAB'))
