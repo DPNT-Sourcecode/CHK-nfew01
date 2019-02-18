16112f5612938bd589bb8e01c34bce1893e5888f
sbx_user1060 <sbx_user1060@ip-10-128-27-25.eu-west-2.compute.internal>
2019-02-18 13:32:12 +0000

    Mon Feb 18 13:32:12 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,15 +5,14 @@
 def checkout(skus):
     total_checkout = []
     item = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+
+    #dealing with single items
     for sku in skus:
         if sku in item.keys():
-            total.append()
+            total.append(item[sku])
+
+    #dealing with special offers
+    
 
 
-
-
-
-
-
-
-
+print(checkout('A,B,C,D'))
